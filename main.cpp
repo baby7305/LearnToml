c219975521a0cd175d29e67833149716fab38a4b
hzzn <songhaibei7305@163.com>
2021-01-25 15:02:28 +0800

    添加代码

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <toml.hpp>
+#include <iostream>
+
+int main()
+{
+    auto data = toml::parse("example.toml");
+
+    // find a value with the specified type from a table
+    std::string title = toml::find<std::string>(data, "title");
+
+    // convert the whole array into any container automatically
+    std::vector<int> nums = toml::find<std::vector<int>>(data, "nums");
+
+    // access with STL-like manner
+    if(data.at("a").contains("b"))
+    {
+        data["a"]["b"] = "c";
+    }
+
+    // pass a fallback
+    std::string name = toml::find_or<std::string>(data, "name", "not found");
+
+    // width-dependent formatting
+    std::cout << std::setw(80) << data << std::endl;
+
+    return 0;
+}
